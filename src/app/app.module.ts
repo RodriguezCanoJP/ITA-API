c9031355af7b974d0347e16df8e9302077631491
JPampers <jp99@estudiantec.cr>
2022-10-04 01:09:09 -0600

    import modules for slider and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { ServicesService } from './Services/services.service';
+import { BooksComponent } from './Components/books.component';
+import { SliderComponent } from './Components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ServicesService],
   bootstrap: [AppComponent]
